da1e6096b084293718a9acf68a817eb2eef52e0f
Khochawongwat <86607408+Khochawongwat@users.noreply.github.com>
2023-05-09 07:44:34 +0000

    I am done!

--- main.c
+++ main.c
@@ -17,7 +17,8 @@ int main()
   
    {  //block start new scope
        int x=7;// hides both x in outer scope and global xor_eqconst_cast
-       printf("Local x in main's inner scope is %d \n",x);
+       printf("Local x in main's inner scope is %d 
+ \n",x);
    }
 
   printf("Local x in main is %d \n",x);
